1f2ca4307eec191160b2855c947ea08de6a110a6
shahida-vs <shahida.khatoon@vectoscalar.com>
2020-06-06 12:16:12 +0530

    middleware

---
+++ middleware/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/time', function (req, res) {
+    res.type('text/plain');
+    req.tm = new Date();
+    res.send({ "message": "displaying time", "time": req.tm });
+})
+app.get('/', function (req, res) {
+    res.type('text/plain');
+    res.send({ "message": "Learning Node Middleware" });
+})
+
+app.use(function (req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send("404 - Page Not Found")
+})
+
+
+app.listen(3005, function () {
+    console.log("Listening on port 3005");
+
+})
